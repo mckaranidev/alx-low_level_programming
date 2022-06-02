ac398b240a9fec9f346fc47a81093b1b83222627
mckaranidev <mckarani.dev@gmail.com>
2022-06-02 06:48:50 -0700

    Fixing errors associated with Betty linter

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,3 +7,13 @@ int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art");
 return (0);}
+#include <stdio.h>
+/**
+ * main - Should print: with proper grammar, but the outcome is a piece of art,
+ * Return: The program returns 0 when it ends
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
