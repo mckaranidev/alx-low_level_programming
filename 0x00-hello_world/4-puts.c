292506b0ba5a533372acd6e7dac41a03762f6607
mckaranidev <mckarani.dev@gmail.com>
2022-06-02 06:25:06 -0700

    Another fix for a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line - 3.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-/* 
-* Description: main - A C program that prints exactly "Programming is like building a multilingual puzzle.
+/*
+* main - Prints exactly "Programming is like building a multilingual puzzle
 * Return: 0 value when the programs ends. 
 */
 
