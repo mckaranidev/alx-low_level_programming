26734e565a69e38b644b3a173e30de14e3089934
mckaranidev <mckarani.dev@gmail.com>
2022-06-02 06:50:47 -0700

    Fixing the overwriting error

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,6 @@
 #include <stdio.h>
 /**
  * main - Should print: with proper grammar, but the outcome is a piece of art,
- * return: The program returns 0 when it ends
- */
-int main(void)
-{
-printf("with proper grammar, but the outcome is a piece of art");
-return (0);}
-#include <stdio.h>
-/**
- * main - Should print: with proper grammar, but the outcome is a piece of art,
  * Return: The program returns 0 when it ends
  */
 int main(void)
