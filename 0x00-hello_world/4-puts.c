f6ad8423dfb177892ed189c03771248e03a32231
mckaranidev <mckarani.dev@gmail.com>
2022-06-02 06:28:44 -0700

    Removing the white trailing space

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,6 @@
 * main - Prints exactly "Programming is like building a multilingual puzzle
 * Return: 0 value when the programs ends. 
 */
-
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
