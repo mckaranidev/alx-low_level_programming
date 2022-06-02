f6eb730644529931a3d96992a211f042d00b803c
mckaranidev <mckarani.dev@gmail.com>
2022-06-02 05:55:21 -0700

    Fixing the Betty coding and documentation style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-/* A C program that prints exactly "Programming is like building a multilingual puzzle", followed by a new line. */
+/** 
+*Description: A C program that prints exactly 
+* "Programming is like building a multilingual puzzle", 
+* followed by a new line. */
 
 int main(){
 puts("\"Programming is like building a multilingual puzzle");
