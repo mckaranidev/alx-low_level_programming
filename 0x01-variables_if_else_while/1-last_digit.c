f9e2bb89daddcb53f33b447cb4fc677bdd458ea2
mckaranidev <mckarani.dev@gmail.com>
2022-06-03 10:23:39 -0700

    Program assigns a random number to the variable n each time it is executed.

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Entry point
+ * main - Program assigns a random number to the variable n each time
  *
- * Return: Always 0 (Success)
+ * Return: Always return 0
  */
 int main(void)
 {
