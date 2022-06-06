11ddbdf73b1d48c409b95ad4413646b804b62c22
mckaranidev <mckarani.dev@gmail.com>
2022-06-06 12:24:47 -0700

    Prints all possible combinations of single-digit numbers in ascending order(numbers must be separated by ,, followed by a space).

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main -  Print all possible combinations of single-digit
+ * Return: 0
+ */
+
+int main(void)
+{
+int combo;
+
+for (combo = 48; combo < 58; combo++)
+{
+putchar(combo);
+if (combo != 57)
+{
+putchar(,);
+putchar( );
+}
+}
+putchar(n);
+return (0);
+}
