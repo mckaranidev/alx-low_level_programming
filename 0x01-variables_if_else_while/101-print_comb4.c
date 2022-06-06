bcf3c5ea0291b3f644d2df121bcafd7435e2e076
Mark Dev <101109571+mckaranidev@users.noreply.github.com>
2022-06-06 23:27:28 +0300

    fix

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,16 +9,16 @@ int comboThree1;
 int comboThree2;
 int comboThree3;
 
-for (comboThree1 = '0'; comboThree1 <= '7'; comboThree1++)
+for (comboThree1 = 0; comboThree1 <= 7; comboThree1++)
 {
-for (comboThree2 = comboThree1 + '1'; comboThree2 <= '9'; comboThree2++)
+for (comboThree2 = comboThree1 + 1; comboThree2 <= 9; comboThree2++)
 {
-for (comboThree3 = comboThree2 + '1'; comboThree3 <= '9'; comboThree3++)
+for (comboThree3 = comboThree2 + 1; comboThree3 < 9; comboThree3++)
 {
 putchar(comboThree1);
 putchar(comboThree2);
 putchar(comboThree3);
-if (comboThree1 == '7' && comboThree2 == '8' && comboThree3 == '9')
+if (comboThree1 == 7 && comboThree2 == 8 && comboThree3 == 9)
 break;
 putchar(',');
 putchar(' ');
