ea22e9075bdd7b5a279ad900e15af5150c9f8344
Mark Dev <101109571+mckaranidev@users.noreply.github.com>
2022-06-06 21:13:05 +0300

    Prints the alphabet in lowercase, and then uppercase, followed by a new line.

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,13 +8,13 @@ int main(void)
 {
 char alphABET;
 
-for (alphABET = a; alphABET <= z; alphABET++)
+for (alphABET = 'a'; alphABET <= 'z'; alphABET++)
 putchar(alphABET);
 
-for (alphABET = A; alphABET <= Z; alphABET++)
+for (alphABET = 'A'; alphABET <= 'Z'; alphABET++)
 putchar(alphABET);
 
-putchar(\n);
+putchar('\n');
 
 return (0);
 }
