d5781093891f5b4eebc1e815bf2b7e2d42ba69f5
Mark Dev <101109571+mckaranidev@users.noreply.github.com>
2022-06-06 22:58:49 +0300

    Prints all possible different combinations of two digits fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,8 +12,8 @@ for (num1 = 0; num1 <= 10; num1++)
 {
 for (num2 = 0; num2 <= 10; num2++)
 {
-putchar((num1 % 10)+ '0');
-putchar(num2 % 10)+ '0');
+putchar((num1 % 10) + '0');
+putchar(num2 % 10) + '0');
 if (num1 == 9 && num2 == '9')
 continue;
 putchar(',');
