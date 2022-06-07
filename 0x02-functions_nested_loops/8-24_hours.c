0a73d461610941ae6590911e36d8ef7bd8f0bae7
Mark Dev <101109571+mckaranidev@users.noreply.github.com>
2022-06-08 00:19:25 +0300

    Function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59 fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,7 +16,7 @@ for (minutes = 0; minutes <= 59; minutes++)
 {
 _putchar((hours / 10) + '0');
 _putchar((hours % 10) + '0');
-_putchar(:);
+_putchar(':');
 _putchar((minutes / 10) + '0');
 _putchar((minutes % 10) + '0');
 _putchar('\n');
