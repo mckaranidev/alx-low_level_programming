1a5b9cff404d2510cc8b39641be7699ea91bce30
mckaranidev <mckarani.dev@gmail.com>
2022-06-10 10:10:16 -0700

    Fixing errors here

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,23 +1,23 @@
-int _putchar(char c);
+#ifndef MAIN_H
 
-int _isupper(int c);
+#define MAIN_H
 
-int _isdigit(int c);
 
-int mul(int a, int b);
 
-void print_numbers(void);
+#include <stdio.h>
 
-void print_most_numbers(void);
 
-void more_numbers(void);
 
-void print_line(int n);
+void positive_or_negative(int i);
 
-void print_diagonal(int n);
+int largest_number(int a, int b, int c);
 
-void print_square(int size);
+void print_remaining_days(int month, int day, int year);
 
-void print_triangle(int size);
+int convert_day(int month, int day);
 
-void print_number(int n);
+void print_times_table(int n);
+
+
+
+#endif
