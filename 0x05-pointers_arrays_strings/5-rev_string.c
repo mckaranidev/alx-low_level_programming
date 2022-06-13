cd54c7a50076c2e4473c3ef5a7f855bb90e8ca0b
mckaranidev <mckarani.dev@gmail.com>
2022-06-13 10:24:00 -0700

    Function that reverses a string.

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* rev_string - Function that reverses a string.
+* @s: The string to be reversed.
+*/
+
+void rev_string(char *s)
+{
+int len = 0, n = 0;
+char tmp;
+
+while (str[n++])
+len++;
+
+for (n = len - 1; n >= len / 2; n--)
+{
+tmp = str[n];
+str[n] = str[len - n - 1];
+str[len - n - 1] = tmp;
+}
+}
