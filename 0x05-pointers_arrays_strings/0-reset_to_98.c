966b5338f5326baaf6438cc7b3f671222d7ac346
Mark Dev <101109571+mckaranidev@users.noreply.github.com>
2022-06-13 19:29:57 +0300

    Correcting betty style

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,27 +1,13 @@
 #include "main.h"
 
-
-
 /**
- *
- * * reset_to_98 - takes a pointer to an int as parameter and updates
- *
- * * the value it points to to 98. Prototype: void reset_to_98(int *n);
- *
- * * @n: pointer to an integer
- *
- * * Return: None
- *
- * */
-
-
+ * reset_to_98 - takes a pointer to an int as parameter and updates
+ * the value it points to 98. Prototype: void reset_to_98(int *n);
+ * @n: pointer to an integer
+ * Return: None
+ */
 
 void reset_to_98(int *n)
-
 {
-
 	*n = 98;
-
 }
-
-
