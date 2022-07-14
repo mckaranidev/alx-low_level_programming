65b2808f7a5f204370b07dd16a8a55c75c4669d4
mckaranidev <mckarani.dev@gmail.com>
2022-07-14 19:00:16 +0000

    Updating files

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,52 +3,52 @@
 #include "lists.h"
 
 /**
-  * *add_node_end - adds a new node at the end of a linked list.
-  * @head: start of the list.
-  * @str: string.
-  * Return: the address of the new element,
-  * or null if it failed.
-  */
+*add_node_end - adds a new node at the end of a linked list.
+* @head: start of the list.
+* @str: string value
+* Return: the address of the new element or NULL if it fails
+*/
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *newNode;
+list_t *newNode;
 
-	newNode = (list_t *)malloc(sizeof(list_t));
-	if (newNode == NULL)
-		return (NULL);
-	newNode->str = strdup(str);
-	newNode->len = string_length(str);
-	newNode->next = NULL;
-	/* check if list is null and make new node the head */
-	if (*head == NULL)
-		*head = newNode;
-	/* else find last node and add new node */
-	else
-	{
-		list_t *lastNode = *head;
-		/* last node has to be NULL use while to check */
-		while (lastNode->next != NULL)
-		{
-			lastNode = lastNode->next;
-		}
-		/* if null is found add new node at the end */
-		lastNode->next = newNode;
-	}
-	return (newNode);
+newNode = (list_t *)malloc(sizeof(list_t));
+if (newNode == NULL)
+return (NULL);
+newNode->str = strdup(str);
+newNode->len = string_length(str);
+newNode->next = NULL;
+
+if (*head == NULL)
+*head = newNode;
+
+else
+{
+list_t *lastNode = *head;
+
+while (lastNode->next != NULL)
+{
+lastNode = lastNode->next;
+}
+
+lastNode->next = newNode;
+}
+return (newNode);
 }
 
 /**
-  * string_length - finds the length of a string.
-  * Return: length of c.
-  * @pointer: pointer.
-  */
+* string_length - finds the length of a string.
+* Return: length of c.
+* @pointer: pointer.
+*/
+
 int string_length(const char *pointer)
 {
-	int c = 0;
+int c = 0;
 
-	while (*(pointer + c) != '\0')
-	{
-		c++;
-	}
-	return (c);
+while (*(pointer + c) != '\0')
+{
+c++;
+}
+return (c);
 }
