15fd69e9ea7be3319ec5c958cc87adfc72419e67
mckaranidev <mckarani.dev@gmail.com>
2022-07-15 13:19:11 +0000

    Add 8-rot13.c fix

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -12,18 +12,18 @@ char *rot13(char *str)
 	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	while (*(s + count) != '\0')
+	while (*(str + count) != '\0')
 	{
 		for (i = 0; i < 52; i++)
 		{
-			if (*(s + count) == alphabet[i])
+			if (*(str + count) == alphabet[i])
 			{
-				*(s + count) = rot13[i];
+				*(str + count) = rot13[i];
 				break;
 			}
 		}
 		count++;
 	}
 
-	return (s);
+	return (str);
 }
